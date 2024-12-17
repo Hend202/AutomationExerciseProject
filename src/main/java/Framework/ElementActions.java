69832a428e2bacf62aa2b3c4f459dd6a6f615890
hind.awad <hendhamed778@gmail.com>
2024-12-17 09:39:49 +0200

    Implemeny Methods

--- src/main/java/Framework/ElementActions.java
+++ src/main/java/Framework/ElementActions.java
@@ -2,6 +2,7 @@ package Framework;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.Wait;
@@ -10,7 +11,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class ElementActions {
-    private static final int DEFAULT_WAITING_TIME = 10;
+    private static final int Default_Waiting_Time = 10;
 
     @Step("Click on Element with Locator {elementLocator}")
     public static void click(WebDriver driver, By elementLocator){
@@ -21,19 +22,50 @@ public class ElementActions {
         String elementName = driver.findElement(elementLocator).getAccessibleName();
         driver.findElement(elementLocator).click();
 
-        System.out.println("Clicking on element " + elementLocator + " " + elementName);
+        System.out.println("Click on element " + elementLocator + " " + elementName);
 
 
     }
+    @Step("Type text into element located by: {elementLocator}")
+    public static void sendKeys(WebDriver driver, By elementLocator, String text) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(Default_Waiting_Time));
+        elementWaiting(driver, wait, elementLocator);
+        WebElement element = driver.findElement(elementLocator);
+        element.clear(); // Clear any existing text
+        element.sendKeys(text);
+        System.out.println("Send keys to located element: " + text);
+    }
+
     @Step("Select Dropdown Option by Visible Text: {elementLocator}")
     public static void selectDropdownByValue(WebDriver driver, By elementLocator, String value) {
-        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(DEFAULT_WAITING_TIME));
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(Default_Waiting_Time));
         elementWaiting(driver, wait, elementLocator);
         Select dropdown = new Select(driver.findElement(elementLocator));
         dropdown.selectByValue(value);
         System.out.println("Selected dropdown option: " + value);
     }
+    @Step("Get Text from Element located by: {elementLocator}")
+    public static String getText(WebDriver driver, By elementLocator) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(Default_Waiting_Time));
+        elementWaiting(driver, wait, elementLocator);
+        return driver.findElement(elementLocator).getText();
+    }
+
+    @Step("Get the title of the current webpage")
+    public static String getTitle(WebDriver driver) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(Default_Waiting_Time));
+        return driver.getTitle();
+    }
+    @Step("Get the Url of the current webpage")
+    public static String getCurrentUrl(WebDriver driver) {
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(Default_Waiting_Time));
+        return driver.getCurrentUrl();
+    }
+
+
+
     private static void elementWaiting(WebDriver driver, Wait<WebDriver> wait, By elementLocator) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(elementLocator));
     }
+
 }
